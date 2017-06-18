5ae1fe35c1b1021b02863202a20c8a0c9712fcc2
Francis <fransterx123@gmail.com>
2017-06-18 17:45:47 -0400

    Added option 1

--- RentACat.java
+++ RentACat.java
@@ -3,11 +3,26 @@ import java.io.*;
 
 public class RentACat {
 	
-	// Globals
+	/**
+	* Globals
+	*/
+
 	private static Scanner keyboard;
+	private static ArrayList<Cat> cats;
+	private static ArrayList<Customer> customers;
 	
+	/**
+	* Simulate the Rent-A-Cat rental system
+	*/
+
 	public static void main(String[] args) {
 	
+		if(!initialize()) { // error dynamically generating cats and customers
+			System.out.println("Program will now exit. ");
+			return;
+		}
+
+
 		while(mainMenu()) {
 			// keep looping
 		}
@@ -19,7 +34,10 @@ public class RentACat {
 	} // End of main
 	
 	
-	// Load the main menu and take user input
+	/**
+	* Load the main menu and take user input
+	*/
+
 	public static boolean mainMenu() {
 		
 		try {
@@ -29,7 +47,7 @@ public class RentACat {
 			String input = keyboard.nextLine();
 			
 			if(input.equals("1")) {
-	
+				listCats();
 			}
 			else if(input.equals("2")) {
 				
@@ -44,16 +62,115 @@ public class RentACat {
 				System.out.println("Please enter a valid option! ");
 				mainMenu();
 			}
-			return true; // fall through
+			return false; // fall through error -- so break out
+
 		} 
 		catch(Exception ex) { // generic error handling
 			
-			System.out.println(ex.toString());
+			ex.printStackTrace();
 			return false;
 			
 		}
 		
 	} // End of main menu display
 	
+
+	public static void listCats() {
+
+		for(Cat cat: cats) {
+
+			if(cat.isAvailable()) {
+				System.out.println(cat.toString());
+			}
+
+		}
+
+	}
+
+	/**
+	* Dynamically generate Cats and Customers
+	*/
+	public static boolean initialize() {
+
+		try {
+
+			cats = new ArrayList<Cat>();
+			customers = new ArrayList<Customer>();
+
+			// top 10 most common cat names in the US
+			String[] possibleCatNames = 
+			{
+				"Bella", "Max", "Chloe", "Oliver", "Lucy",
+				"Charlie", "Lily", "Sophie", "Tiger", "Shadow"
+			};
+
+			// top 10 most common first names in the US
+			String[] possibleFirstNames = 
+			{
+				"James", "John", "Robert", "Michael", "William",
+				"Mary", "Patricia", "Linda", "Barbara", "Elizabeth"
+			};
+
+			// top 10 most common last names in the US
+			String[] possibleLastNames = 
+			{
+				"Smith", "Johnson", "Williams", "Brown", "Jones",
+				"Miller", "Davis", "Garcia", "Rodriguez", "Wilson"
+			};
+
+			Random random = new Random();
+
+			// max and min number of cats and customers allowed to be in the system
+			int low = 2, 
+				high = 6;
+
+			// dynamically generate how many cats and customers will be in the system
+			int numOfCats = random.nextInt(high - low) + low,
+				numOfCustomers = random.nextInt(high - low) + low;
+
+			// dynamically generate indices to help pick cat and customer names
+			int[] catNames = random.ints(1, 11).distinct().limit(numOfCats).toArray(),
+				firstNames = random.ints(1, 11).distinct().limit(numOfCustomers).toArray(),
+				lastNames = random.ints(1, 11).distinct().limit(numOfCustomers).toArray();	
+
+			// load cats
+			for(int i = 0; i < numOfCats; i++) {
+
+				/* 
+				* dynamically generate the current cat's rental rate
+				* the min is $100.00 and the max is $500.00
+				*
+				* the innermost calculation finds the rate
+				*
+				* the wrapping Math.round() call and operations 
+				* will round the rate to the nearest 10's value
+				*
+				* ex. original rate = 369.51 will become 370.00
+				*/
+				double rate = Math.round( (100.00 + Math.random() * (500.00 - 100.00) ) /10) * 10;
+
+				cats.add(new Cat(possibleCatNames[catNames[i]-1], i+1, rate));
+
+			}
+
+			// load customers
+			for(int i = 0; i < numOfCustomers; i++) {
+
+				customers.add(new Customer(possibleFirstNames[firstNames[i]-1], possibleLastNames[lastNames[i]-1], i+1));
+
+			}
+
+			return true;
+
+		}
+		catch(Exception ex) { // generic error handling
+
+			ex.printStackTrace();
+			return false;
+
+		}
+
+	} // End of initialize Cats and Customers
+
 	
 } // End of class
